1009b65c2a7a1c43b1615f9d275e0517fa8e2872
suprimentosdg <157532344+suprimentosdg@users.noreply.github.com>
2024-02-09 16:50:17 -0300

    Update Contatos.py

--- pages/Contatos.py
+++ pages/Contatos.py
@@ -10,4 +10,4 @@ st.markdown("[Clique aqui](https://api.whatsapp.com/send/?phone=8488710199&text&
 st.markdown("[Clique aqui](https://api.whatsapp.com/send/?phone=7181964547&text&type=phone_number&app_absent=0) para falar com **Beatriz Rozendo** (ADM da Bahia).")
 st.markdown("[Clique aqui](https://api.whatsapp.com/send/?phone=9188394333&text&type=phone_number&app_absent=0) para falar com **Vinícius Pantoja** (ADM do Pará).")
 st.markdown("Telefone fixo do setor de Suprimentos **(86) 3194-7558**.")
-st.markdown("[Clique aqui](https://api.whatsapp.com/send/?phone=8695450287&text&type=phone_number&app_absent=0) para falar com o Coordenador do Setor **Airton Leal** (ADM do Pará).")
+st.markdown("[Clique aqui](https://api.whatsapp.com/send/?phone=8695450287&text&type=phone_number&app_absent=0) para falar com o Coordenador do Setor **Airton Leal**.")
