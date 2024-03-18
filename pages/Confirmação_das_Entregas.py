3d1e93cc4a4192a6d5de12fdbea3761e27ec0996
suprimentosdg <157532344+suprimentosdg@users.noreply.github.com>
2024-03-18 16:36:30 -0300

    Update Confirmação_das_Entregas.py

--- pages/Confirmação_das_Entregas.py
+++ pages/Confirmação_das_Entregas.py
@@ -12,31 +12,31 @@ col1, col2 = st.columns([6, 1])
 col1.title("Portal de Suprimentos")
 image_path = "logo_globo.png"
 image = col2.image(image_path, width=80)
-st.subheader("Confirmação de entrega")
+st.subheader("Confirmação de Entrega")
 
 regions = ["Piauí", "Maranhão", "Rio Grande do Norte", "Bahia", "Pará"]
 suppliers = ["Atlas Papelaria", "Atakadinho Bahia", "Autopel", "Brilhante", "Casa Norte", "Distribuidora Teresina", "Ecopaper", "J D R Sousa", "L C Vaz", "Macropack", "Mix Comércio", "Nacional", "Shopping Hospitalar", "Supermercado São Jorge (JB)"]
 
 with st.form(key="include_confirmation", clear_on_submit=True):
-    name1 = st.text_input("Digite seu nome:", )
-    region1 = st.selectbox("Selecione sua regional:", regions)
-    store1 = st.number_input("Digite sua loja:", min_value=0, max_value=999)
+    name = st.text_input("Digite seu nome:", )
+    region = st.selectbox("Selecione sua Regional:", regions)
+    store = st.number_input("Digite sua Loja:", min_value=0, max_value=999)
     supplier = st.selectbox("Selecione o Fornecedor:", suppliers)
-    date = st.date_input("Data do recebimento:", format="DD/MM/YYYY")
+    date = st.date_input("Data do Recebimento:", format="DD/MM/YYYY")
     nf = st.number_input("N° da Nota Fiscal:", step=1, format="%d")
-    send1 = st.form_submit_button("Enviar")
+    send1 = st.form_submit_button("Enviar Confirmação")
     if send1:
         formatted_date = datetime.strftime(date, "%d/%m/%Y")
         current_timestamp = datetime.now().timestamp()
         formatted_timestamp = datetime.fromtimestamp(current_timestamp).strftime("%d/%m/%Y %H:%M:%S")
 
-        if not name1 or not region1 or not store1 or not supplier or not date or not nf:
+        if not name or not region or not store or not supplier or not date or not nf:
             st.warning("Por favor, preencha todos os campos.")
         else:
             data_to_insert = {
-                'nome': name1,
-                'regional': region1,
-                'loja': store1,
+                'nome': name,
+                'regional': region,
+                'loja': store,
                 'fornecedor': supplier,
                 'data_recebimento': formatted_date,
                 'nf': nf,
